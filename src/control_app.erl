604c38910c521d0b0ae0d681eed1f4070d86743a
joq62 <jakim.leche@gmail.com>
2023-03-21 21:15:47 +0100

    ud

--- src/control_app.erl
+++ src/control_app.erl
@@ -10,7 +10,8 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    {ok,ClusterSpec}=sd:call(etcd,db_pod_desired_state,read,[cluster_spec,node()],5000),
+ %   {ok,ClusterSpec}=sd:call(etcd,db_pod_desired_state,read,[cluster_spec,node()],5000),
+    {ok,ClusterSpec}=sd:call(etcd,db_cluster_spec,read_cookie,[cluster_spec,erlang:get_cookie()],5000),
     {ok,_}=control_sup:start_link(ClusterSpec).
 
 
